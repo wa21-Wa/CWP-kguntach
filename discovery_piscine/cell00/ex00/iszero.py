cdf25925b884922d7e9bdd2a20a157aefb8a87d6
wa21-Wa <68050037@kmitl.ac.th>
2026-09-22 13:46:37 +0700

    2.0

--- discovery_piscine/cell00/ex00/iszero.py
+++ discovery_piscine/cell00/ex00/iszero.py
@@ -1,5 +1,5 @@
 num = input("Enter a number: ")
-if (num == "0"):
+if num == "0":
     print("This number is equal to zero.")
-if (num > "0"):
+elif num > "0":
     print("This number is different from zero.")
