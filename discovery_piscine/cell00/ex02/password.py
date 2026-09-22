5f624ab3ed91f3ec733dc1bd15db5aa3a3446334
wa21-Wa <68050037@kmitl.ac.th>
2026-09-22 13:48:40 +0700

    2.2

--- discovery_piscine/cell00/ex02/password.py
+++ discovery_piscine/cell00/ex02/password.py
@@ -1,6 +1,6 @@
 password = "Python is awesome"
 str = input("Enter the password: ")
-if (str == password):
+if str == password:
     print("ACCESS GRANTED")
-if (str != password):
+else:
     print("ACCESS DENIED")
