0a8574449d35799a5803ac6dfde043826a242e11
wa21-Wa <68050037@kmitl.ac.th>
2026-09-22 13:21:29 +0700

    2.1

---
+++ discovery_piscine/cell00/ex01/isneg.py
@@ -0,0 +1,10 @@
+num = input("Enter a number: ")
+
+if (num == "0"):
+    print("This number is both positive and negative.")
+    
+if (num < "0"):
+    print("This number is negative.")
+    
+if (num > "0"):
+    print("This number is positive.")
