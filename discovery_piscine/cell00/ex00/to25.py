55f36659cee3b3df436c3fc68a46c1f3800b3e04
wa21-Wa <68050037@kmitl.ac.th>
2026-09-22 13:42:16 +0700

    3.0

---
+++ discovery_piscine/cell00/ex00/to25.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number less than 25: "))
+if num > 25:
+    print("Error")
+else:
+    while num <= 25:
+        print(f"Inside the loop, my variable is {num}")
+        num += 1
