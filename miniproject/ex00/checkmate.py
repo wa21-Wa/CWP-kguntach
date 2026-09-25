989c28c26898a1a49d654ed73912044a1d69fd1d
wa21-Wa <68050037@kmitl.ac.th>
2026-09-25 13:01:17 +0700

    ݁ ˖Ი𐑼⋆

--- miniproject/ex00/checkmate.py
+++ miniproject/ex00/checkmate.py
@@ -67,10 +67,10 @@ def checkmate(board):
                 for dr, dc in directions:
                     rr, cc = r + dr, c + dc
                     while 0 <= rr < n and 0 <= cc < n:
-                        if grid[rr][cc] not in ('.', ' '):
+                        if grid[rr][cc] in PIECES or grid[rr][cc] == 'K':
                             if (rr, cc) == king_pos:
                                 is_check = True
-                            break
+                                break
                         rr += dr
                         cc += dc
 
